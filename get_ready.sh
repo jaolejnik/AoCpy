72ee9e8cc2664ae59de4924ecd93150cb403438b
Jakub Olejnik <olejnik.jakub71@gmail.com>
2021-11-30 23:39:33 +0100

    automated downloading input

---
+++ get_ready.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+day=$(date +"%d")
+year=$(date +"%Y")
+day_dir="day$(printf '%02d' $day)"
+
+if [[ -d ${day_dir} ]]
+then
+    exit 1
+fi
+
+url="https://adventofcode.com/${year}/day/${day}"
+
+firefox --new-window ${url}
+wget -q --load-cookies=cookies.txt -P ${day_dir} "${url}/input"
+touch "${day_dir}/${day_dir}.py"
+
